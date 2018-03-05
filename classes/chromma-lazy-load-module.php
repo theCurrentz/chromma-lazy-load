00b8c885da42de964b714224d0c8daef7b1366a8
root <timbralpw@gmail.com>
2018-03-05 11:37:59 -0800

    fade-in src/srcset fix

--- classes/chromma-lazy-load-module.php
+++ classes/chromma-lazy-load-module.php
@@ -5,17 +5,24 @@ abstract class Chromma_Lazy_Load_Module
   public static function content_lazyload_filter( $content )
   {
       $load_effect = get_option( 'chromma_loadeffect' );
-      (get_option( 'chromma_loadeffect' ) !== "fadein") ? $lowest_dimension_mod = get_option('chromma-load-dimensions') : $lowest_dimension_mod = "";
+      ($load_effect !== "fadein") ? $lowest_dimension_mod = get_option('chromma-load-dimensions') : $lowest_dimension_mod = "";
   		preg_match_all('/<img .*>/iU', $content, $imgMatches);
 
   		foreach ($imgMatches[0] as $imgMatch)
   		{
+          //for each match of images, get matches of src attribute
+					preg_match('/srcset=\"(?<src>.*)\s/iU',$imgMatch, $srcMatch);
 
-  					preg_match('/srcset=\"(?<src>.*)\s/iU',$imgMatch, $srcMatch);
-
-  						//find replace sourcese with a data bound srcset
-  						if (!empty($srcMatch['src']))
-  						{
+					//find replace sourcese with a data bound srcset
+					if (!empty($srcMatch['src']))
+					{
+              if($load_effect == 'fadein')
+              {
+                $imgMatchModifiedsrc = preg_replace('/src=\".*\"/iU' , "src=''", $imgMatch);
+                $imgMatchModified = str_replace("srcset=", "srcset='' data-srcset=", $imgMatchModifiedsrc);
+              }
+              else
+              {
                 //default length of the format(e.g. .jpg, .png) type suffix
                 $formatLength = 4;
                 //if .jpeg, extend to 5
@@ -38,16 +45,18 @@ abstract class Chromma_Lazy_Load_Module
   							if ($srcMatchGif !== false)
   							{
   								$imgMatchModifiedsrc = preg_replace('/src=\".*\"/iU' , "src='{$srcMatch}'", $imgMatch);
+                  $imgMatchModified = str_replace("srcset=", "srcset='{$srcMatchSub}{$srcMatchSubEnd}' data-srcset=", $imgMatchModifiedsrc);
   							}
                 //otherwise
                 else
                 {
-  							$imgMatchModifiedsrc = preg_replace('/src=\".*\"/iU' , "src='{$srcMatchSub}{$srcMatchSubEnd}'", $imgMatch);
+  							   $imgMatchModifiedsrc = preg_replace('/src=\".*\"/iU' , "src='{$srcMatchSub}{$srcMatchSubEnd}'", $imgMatch);
+                   $imgMatchModified = str_replace("srcset=", "srcset='{$srcMatchSub}{$srcMatchSubEnd}' data-srcset=", $imgMatchModifiedsrc);
   							}
-  							$imgMatchModified = str_replace("srcset=", "srcset='{$srcMatchSub}{$srcMatchSubEnd}' data-srcset=", $imgMatchModifiedsrc);
 
   							$content = str_replace($imgMatch, "{$imgMatchModified}", $content);
-  						}
+              }
+					}
   		}
   		$content = str_replace('<img class="', '<img class="lazyload-img llreplace ', $content);
   		return $content;
