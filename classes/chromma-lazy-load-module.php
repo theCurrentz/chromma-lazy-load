d1c02e51be48eb712bd903fca8c9bcb1f4f5bb53
root <root@atl-staging.(none)>
2018-07-09 10:42:31 -0700

    Renaming assets folder to src

--- classes/chromma-lazy-load-module.php
+++ classes/chromma-lazy-load-module.php
@@ -1,116 +1,87 @@
 <?php
 //abstract interface for chromma lazy load
-abstract class Chromma_Lazy_Load_Module
+class Chromma_Lazy_Load_Module
 {
-  public static function content_lazyload_filter( $content )
-  {
-      $load_effect = get_option( 'chromma_loadeffect' );
-      $lowest_dimension_mod = ($load_effect !== "fadein") ? get_option('chromma-load-dimensions') : "";
-      $aspect_ratio =  get_option('chromma-load-ar');
 
-      //match all images
-  		preg_match_all('/<img .*>/iU', $content, $imgMatches);
+  public function __construct() {
+  }
 
-      //iterating through all the images, swap the image src and srcset attributes with data-src and data-srcset
-  		// foreach ($imgMatches[0] as $imgMatch) {
-      //     //for each match of images, get matches of src attribute
-			// 		//find replace sourcese with a data bound srcset
-			// 		if (preg_match('/src=\"(?<src>.*)\s/iU',$imgMatch, $srcMatch))
-			// 		{
-      //         if($load_effect == 'fadein')
-      //         {
-      //           $data_src = "data-src='".$srcMatch['src']."'";
-      //           $imgMatchModifiedsrc = preg_replace('/src=\".*\"/iU' , $data_src, $imgMatch);
-      //           $imgMatchModified = str_replace("srcset=", "srcset='' data-srcset=", $imgMatchModifiedsrc);
-      //         }
-      //         else
-      //         {
-      //           //default length of the format(e.g. .jpg, .png) type suffix
-      //           $formatLength = 4;
-      //           //if .jpeg, extend to 5
-      //           if(strpos($srcMatch['src'], 'jpeg') == true)
-      //           {
-      //             $formatLength = 5;
-      //           }
-      //
-  		// 					//store length of source
-  		// 					$srcMatchLength = strlen($srcMatch['src']);
-  		// 					//if grab everything except the last 4 characters
-  		// 					$srcMatchSub = substr( $srcMatch['src'] , 0 , ($srcMatchLength - $formatLength) );
-  		// 					//detect gif format
-  		// 					$srcMatchGif = strpos($srcMatch['src'], 'gif');
-      //
-  		// 					//attach the a modifier for lowest dimensions available to you directly before the format(e.g. .jpg) type suffix
-  		// 					$srcMatchSubEnd = $lowest_dimension_mod . substr( $srcMatch['src'], $srcMatchLength - $formatLength, ( $srcMatchLength ) );
-      //
-      //           //if src is a gif, do not to attach lowest dimension modifer
-  		// 					if ($srcMatchGif !== false) {
-  		// 						$imgMatchModifiedsrc = preg_replace('/src=\".*\"/iU' , "src='{$srcMatch}' data-src='{$srcMatch}'", $imgMatch);
-      //             $imgMatchModified = str_replace("srcset=", "srcset='{$srcMatchSub}{$srcMatchSubEnd}' data-srcset=", $imgMatchModifiedsrc);
-  		// 					}
-      //           else {
-  		// 					   $imgMatchModifiedsrc = preg_replace('/src=\".*\"/iU' , "src='{$srcMatchSub}{$srcMatchSubEnd}'", $imgMatch);
-      //              $imgMatchModified = str_replace("srcset=", "srcset='{$srcMatchSub}{$srcMatchSubEnd}' data-srcset=", $imgMatchModifiedsrc);
-  		// 					}
-      //         }
-      //         $content = str_replace($imgMatch, "{$imgMatchModified}", $content);
-			// 		}
-  		// }
-      $content = mb_convert_encoding($content, 'HTML-ENTITIES', "UTF-8");
-      $dom = new DOMDocument();
-      $dom->loadHTML($content);
-      $dom->encoding = 'utf-8';
-      $xpath = new DOMXpath($dom);
-      //xpath query targets all images that are children of the div css class entry-content
-      $unwrappedImgs = $xpath->query("//img");
-      foreach ($unwrappedImgs as $img) {
-        //create a figure & set class to entry-content_figure
+  public static function apply_aspect_ratio($img, $figure) {
+    //gather options
+    $load_effect = get_option( 'chromma_loadeffect' );
+    $lowest_dimension_mod = ($load_effect !== "fadein") ? get_option('chromma-load-dimensions') : "";
+    $aspect_ratio =  get_option('chromma-load-ar');
+    //parse out the desired dimensions and apply the dimensions as an aspect ratio to the figure
+    $aspect_ratio = str_replace('x', ',', $aspect_ratio);
+    $aspect_ratio = str_replace('-', '', $aspect_ratio);
+    $aspect_ratio_array = explode(',', $aspect_ratio);
+    $width = $aspect_ratio_array[0];
+    $height = $aspect_ratio_array[1];
+    //list($width, $height) = getimagesize($src);
+    $aspectRatio = ($height > 0 && $width > 0) ? ($height / $width) * 100 : 101;
+
+    //if aspect ratio is larger than desired, we'll fallback to a figure/img relationship w/o a set aspec ratio
+    if($aspectRatio > 80 ) {
+       $img->setAttribute('style', 'position: relative');
+    }
+    $aspectThresholdfix = ($aspectRatio > 58 && !($figure->getAttribute('class') )) ? 'height: auto; padding: 0px; max-height: '.$height.'px; max-width: '.$width.'px;' : 'padding-bottom: '. $aspectRatio .'%;';
+    $styles = $aspectThresholdfix;
+    $figure->setAttribute('style', $styles);
+  }
+
+  public static function content_lazyload_filter( $content ) {
+    //initialize a dom document for easier more accurate parsing
+    $content = mb_convert_encoding($content, 'HTML-ENTITIES', "UTF-8");
+    $dom = new DOMDocument();
+    $dom->loadHTML($content);
+    $dom->encoding = 'utf-8';
+    $xpath = new DOMXpath($dom);
+    //xpath query targets all images that are children of the div css class entry-content
+    $unwrappedImgs = $xpath->query("//img");
+    foreach ($unwrappedImgs as $img) {
+      //create a figure & set class to entry-content_figure
+      if (!($img->parentNode->nodeName == 'figure')) {
         $figure = $dom->createElement('figure');
         $figure->setAttribute('class','entry-content_figure');
-
         //replace $img with wrapper figure then appendChild the $img back into the figure
         $img->parentNode->replaceChild($figure, $img);
         $figure->appendChild($img);
-
-        //parse out the desired dimensions and apply the dimensions as an aspect ratio to the figure
-        $aspect_ratio = get_option('chromma-load-ar');
-        $aspect_ratio = str_replace('x', ',', $aspect_ratio);
-        $aspect_ratio = str_replace('-', '', $aspect_ratio);
-        $aspect_ratio_array = explode(',', $aspect_ratio);
-        $width = $aspect_ratio_array[0];
-        $height = $aspect_ratio_array[1];
-        //list($width, $height) = getimagesize($src);
-        $aspectRatio = ($height > 0 && $width > 0) ? ($height / $width) * 100 : 101;
-
-        //if aspect ratio is larger than desired, we'll fallback to a figure/img relationship w/o a set aspec ratio
-        if($aspectRatio > 80 ) {
-           $img->setAttribute('style', 'position: relative');
-        }
-        $aspectThresholdfix = ($aspectRatio > 58) ? 'height: auto; padding: 0px; max-height: '.$height.'px; max-width: '.$width.'px;' : 'padding-bottom: '. $aspectRatio .'%;';
-        $styles = $aspectThresholdfix;
-        $figure->setAttribute('style', $styles);
-
-        //set img src to a blank transparent gif
-        $imgSrc = $img->getAttribute('src');
-        $imgSrcSet = $img->getAttribute('srcset');
-        $img->removeAttribute('srcset');
-        $img->setAttribute('data-src', $imgSrc);
-        if (!empty($imgSrcSet)) {
-          $img->setAttribute('data-srcset', $imgSrcSet);
-        }
-        $img->setAttribute('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
-        $imgClasses = $img->getAttribute('class') . ' lazyload-img llreplace';
-        $img->setAttribute('class', $imgClasses);
-
-        //save the node to the $dom object
-        $dom->saveHTML($img);
-        $dom->saveHTML($figure);
+      } else {
+        $figure = $img->parentNode;
       }
 
-      //save and return
-      $content = preg_replace('/^<!DOCTYPE.+?>/','',str_replace(array('<html>', '</html>', '<body>', '</body>'),array('', '', '', ''),$dom->saveHTML()));
-      return $content;
+      //check for exemptions
+      $imgClasses = (string)$img->getAttribute('class');
+      if (strpos($imgClasses,'size-full') == false) {
+        self::apply_aspect_ratio($img, $figure);
+      } else {
+        list($width_full, $height_full) = getimagesize($img->getAttribute('src'));
+        $aspectRatio_full = ( $height_full > 0 && $width_full > 0) ? ($height_full / $width_full) * 100 : auto;
+        $figure->setAttribute('style', "padding-bottom: " . $aspectRatio_full . "%");
+      }
 
+      //set img src/datasrc for lazyload handling
+      $imgSrc = $img->getAttribute('src');
+      $imgSrcSet = $img->getAttribute('srcset');
+      $img->removeAttribute('srcset');
+      $img->setAttribute('data-src', $imgSrc);
+      if (!empty($imgSrcSet)) {
+        $img->setAttribute('data-srcset', $imgSrcSet);
+      }
+      //set img src to a blank transparent gif
+      $img->setAttribute('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+      $imgClasses = $img->getAttribute('class') . ' lazyload-img llreplace';
+      $img->setAttribute('class', $imgClasses);
+
+      //save the node to the $dom object
+      $dom->saveHTML($img);
+      $dom->saveHTML($figure);
+    }
+
+    //save and return
+    $content = preg_replace('/^<!DOCTYPE.+?>/','',str_replace(array('<html>', '</html>', '<body>', '</body>'),array('', '', '', ''),$dom->saveHTML()));
+    return $content;
   }
 
-}
+
+} //end Chromma_Lazy_Load_Module
---
+++ src/lazy-load.js
@@ -0,0 +1,60 @@
+// fire when dom content is loaded
+if(document.getElementsByClassName('llreplace').length > 0) {
+  var allimages = document.getElementsByClassName('llreplace')
+  allimages[allimages.length - 1].addEventListener('load', lazyLoadController())
+}
+
+function lazyLoadController () {
+  // lazyloading images
+  var allimages = document.getElementsByClassName('llreplace')
+  lazyLoadExecution() //fire once before scroll listener
+  if (allimages.length > 0) {
+    window.addEventListener('scroll', function () {
+      lazyLoadExecution()
+    })
+  }
+
+  // execute lazy load
+  function lazyLoadExecution() {
+    var imgCount = 0
+    while (imgCount < allimages.length) {
+      if (inView(allimages[imgCount])) {
+        lazyLoadImage(allimages[imgCount])
+        allimages[imgCount].classList.remove('llreplace')
+      } else {
+        imgCount++
+      }
+    }
+  }
+
+}
+
+// discover if the lazyload target element is in view
+function inView (element) {
+  var elementSize = element.getBoundingClientRect()
+  var html = document.documentElement
+  return (
+    elementSize.top >= 0 &&
+        elementSize.left >= -100 &&
+        elementSize.bottom <= 620 + (window.innerHeight || html.clientHeight) &&
+        elementSize.right <= 2640 + (window.innerWidth || html.clientWidth)
+  )
+}
+
+// replace src w/ data src and animate image in
+function lazyLoadImage (element) {
+  if (element.getAttribute('data-srcset')) {
+    element.setAttribute('srcset', element.getAttribute('data-srcset'))
+    element.removeAttribute('data-srcset')
+  }
+  if (element.getAttribute('data-src')) {
+    element.setAttribute('src', element.getAttribute('data-src'))
+    element.removeAttribute('data-src')
+  }
+  // use below code if using a container e.g. figure
+  // element.style.position = "relative";
+  // element.style.height = "auto";
+  // element.parentNode.style.height = "auto";
+  // element.parentNode.style.paddingBottom = "0px";
+  if (element.classList) { element.classList.add('reveal') }
+}
